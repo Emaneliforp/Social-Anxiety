f3572b7914816512ade4f67aba88e88e4317d0fc
lillee205 <leelillian205@gmail.com>
2018-08-18 11:28:28 -0700

    gotta push to pull

--- public/signup.js
+++ public/signup.js
@@ -7,10 +7,38 @@ var config = {
     messagingSenderId: "25285430457"
   };
   firebase.initializeApp(config);
+
 const FIREBASE_DATABASE = firebase.database();
 const FIREBASE_AUTH = firebase.auth();
 
-const name = document.getElementById("name_field");
-const age = document.getElementById("age_field");
-const email = document.getElementById("email_field");
-const password = document.getElementById("password_field");
+const tName = document.getElementById("name_field");
+const tAge = document.getElementById("age_field");
+const tEmail = document.getElementById("email_field");
+const tPassword = document.getElementById("password_field");
+const tRePassword = document.getElementById("repassword_field");
+let signUpButton = "nah";
+
+setTimeout(function(){
+   signUpButton = document.getElementById("signUpBtn");
+
+ }, 500);
+setTimeout(function(){
+  signUpButton.addEventListener("click", e=> {
+    email = tEmail.value;
+    pass = tPassword.value;
+    const user = {
+      name: tName.value,
+      age: tAge.value
+    };
+    console.log("hello");
+    if (tPassword.value == tRePassword.value){
+      FIREBASE_AUTH.createUserWithEmailAndPassword(email, pass);
+      FIREBASE_DATABASE.ref('users/' + user.uid).set(user);
+      console.log("created");
+    }
+    else{
+      alert("Passwords don't match.");
+      tPass
+    }
+  });
+}, 500);
