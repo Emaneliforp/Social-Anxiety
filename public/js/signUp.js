247bce4a1c3934246409cea33c8b25f30b63db56
lillee205 <leelillian205@gmail.com>
2018-08-30 13:43:05 -0700

    login + signup merge

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -3,21 +3,32 @@ const FIREBASE_DATABASE = firebase.database();
 const FIREBASE_AUTH = firebase.auth();
 
 const tName = document.getElementById("name_field");
-const tAge = document.getElementById("age_field");
+const tGrade = document.getElementById("grade_field");
 const tEmail = document.getElementById("email_field");
 let tPassword = document.getElementById("password_field");
 let tRePassword = document.getElementById("repassword_field");
 
 let signUpButton = document.getElementById("signUpBtn");
+let loginButton =document.getElementById("loginBtn");
 
+loginButton.addEventListener("click", function(){
+  window.location.href = "login.html";
+});
+console.log(tName.value);
 
 signUpButton.addEventListener("click", e=> {
+  //if fields are empty, then stop from creating account
+  if ((tName.value==null) || (tEmail.value==null) || (tPassword.value==null) || (tGrade.value==null) || (tRePassword.value==null)){
+    alert("Please fill out all fields.");
+  }
+  else{
   const EMAIL = tEmail.value;
   const PASSWORD = tPassword.value;
   const USERACC = {
     name: tName.value,
-    age: tAge.value
+    grade: tGrade.value
   };
+
 
   if (tPassword.value == tRePassword.value){
     FIREBASE_AUTH.createUserWithEmailAndPassword(EMAIL, PASSWORD).then(function(){
@@ -35,6 +46,7 @@ signUpButton.addEventListener("click", e=> {
     tPassword.value = "";
     tRePassword.value = "";
   }
+}
 });
 
 
