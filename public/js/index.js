022a2f4f73b93b1b0743ba3c6930b3c44dcf557e
lillee205 <leelillian205@gmail.com>
2018-09-01 12:13:39 -0700

    added more login stuff + index

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,24 @@
 var signUpButtona = document.getElementById("signup");
 
 signUpButtona.addEventListener("click", function(){
-  window.location.href = "signUp.html";
+  window.location.href = "signUpNew.html";
 });
 
 var loginButtona = document.getElementById("loginbtn");
 
 loginButtona.addEventListener("click", function(){
-  window.location.href = "login.html";
+  window.location.href = "signUpNew.html";
+});
+
+$(function() {
+   $(window).scroll(function () {
+      if ($(this).scrollTop() > 50) {
+         $("triangle-down").css("border-top", "100px solid yellow");
+
+      }
+      if ($(this).scrollTop() < 50) {
+        $("triangle-down").css("border-top", "100px solid green");
+
+      }
+   });
 });
