762ae228d67df4e5aa6f62fc92cc10366696a41f
lillee205 <leelillian205@gmail.com>
2018-09-01 13:16:38 -0700

    triangle

--- public/js/index.js
+++ public/js/index.js
@@ -9,16 +9,3 @@ var loginButtona = document.getElementById("loginbtn");
 loginButtona.addEventListener("click", function(){
   window.location.href = "signUpNew.html";
 });
-
-$(function() {
-   $(window).scroll(function () {
-      if ($(this).scrollTop() > 50) {
-         $("triangle-down").css("border-top", "100px solid yellow");
-
-      }
-      if ($(this).scrollTop() < 50) {
-        $("triangle-down").css("border-top", "100px solid green");
-
-      }
-   });
-});
