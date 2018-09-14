b2732bc94259b85a8c3623c2af9db458cac05387
kaytec <kcperidot@gmail.com>
2018-09-13 17:27:49 -0700

    added transition

---
+++ public/js/transition.js
@@ -0,0 +1,5 @@
+setTimeout(
+    function(){
+        document.getElementById('transition').style.display = 'none';
+        document.getElementById('actualpg').style.display = 'block';
+    }, 2900);
