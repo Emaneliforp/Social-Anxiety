f14b98e3e0603738af5eb31074eb4da15faa4907
Emaneliforp <kietchouam@gmail.com>
2018-09-27 20:10:09 -0700

    finish sending messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,15 +9,26 @@ let name = "";
 let m = [];
 let x = 0;
 let pm = [];
-let rmess= {};
+let rmess = {};
+var botc = document.createElement('div');
+
+let chatbox = document.getElementById('chatbox');
 
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         console.log(user);
         name = user.displayName;
         FIREBASE_DATABASE.ref("communities/" + chat).once('value').then(function (snapshot) {
-            pm = snapshot.val().pm;
-            console.log(chat);
+            pm = snapshot.val().m;
+            
+            for (var i = 0; i <= pm.lenght; i++) {
+                console.log('hi');
+                var pc = document.createElement('div');
+                pc.classList.add('bot');
+                pc.textContent = pm[i];
+                chatbox.appendChild(chat);
+                console.log(pm);
+            }
         });
         enterMessage.addEventListener("keyup", function (event) {
             event.preventDefault();
@@ -26,12 +37,13 @@ firebase.auth().onAuthStateChanged(function (user) {
                 mess = document.getElementById("enterMessage").value;
                 m.push(name + ':' + mess);
                 console.log(m);
-                FIREBASE_DATABASE.ref('/communities/testing/chat/' + x).set({
+                FIREBASE_DATABASE.ref('communities/' + chat).set({
                     m
                 });
                 enterMessage.value = "";
             }
         });
+
     } else {
         window.location.href = "index.html";
     }
