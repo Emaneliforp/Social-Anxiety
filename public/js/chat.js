b41bfa8f44c82adb28ed2a14c86b08e7ee52b930
Emaneliforp <kietchouam@gmail.com>
2018-09-27 18:12:30 -0700

    finished creating new chat page

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,30 +3,37 @@ const FIREBASE_DATABASE = firebase.database();
 const FIREBASE_AUTH = firebase.auth();
 
 let enterMessage = document.getElementById("enterMessage");
+let chat = window.localStorage.getItem('chat');
 
 let name = "";
-let token = [];
+let m = [];
 let x = 0;
+let pm = [];
+let rmess= {};
 
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         console.log(user);
         name = user.displayName;
-
+        FIREBASE_DATABASE.ref("communities/" + chat).once('value').then(function (snapshot) {
+            pm = snapshot.val().pm;
+            console.log(chat);
+        });
+        enterMessage.addEventListener("keyup", function (event) {
+            event.preventDefault();
+            if (event.keyCode === 13) {
+                console.log(enterMessage.value);
+                mess = document.getElementById("enterMessage").value;
+                m.push(name + ':' + mess);
+                console.log(m);
+                FIREBASE_DATABASE.ref('/communities/testing/chat/' + x).set({
+                    m
+                });
+                enterMessage.value = "";
+            }
+        });
     } else {
         window.location.href = "index.html";
     }
 });
 
-
-enterMessage.addEventListener("keyup", function (event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        console.log(enterMessage.value);
-        mess = document.getElementById("enterMessage").value;
-        token = [name, mess];
-        FIREBASE_DATABASE.ref('/communities/testing/chat/' + x).set({
-            token: token
-        });
-    }
-});
